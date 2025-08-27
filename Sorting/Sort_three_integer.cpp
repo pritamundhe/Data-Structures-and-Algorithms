cd8052853d7d1edf2da096bd0d0e612ada3fe9fb
Pritam Mundhe <pritammundhe00@gmail.com>
2025-08-27 18:44:47 +0530

    27-08-2025 18:44

---
+++ Sorting/Sort_three_integer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a > b) swap(a, b);
+    if (a > c) swap(a, c);
+    if (b > c) swap(b, c);
+    
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
+
+/*
+Sample Input
+5 2 7
+
+Your Output
+2 5 7
+*/
