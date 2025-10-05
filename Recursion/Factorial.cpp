b9e4e77cbb9e20394462599cd88aab1ecf2b31d3
Pritam Mundhe <pritammundhe00@gmail.com>
2025-10-05 21:22:59 +0530

    05-10-2025 21:22

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int Factorial(int n) {
+    // Write your code here
+    if(n==1){
+        return 1;
+    }
+    return n*Factorial(n-1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << Factorial(n) << endl;
+    return 0;
+}
---
+++ Recursion/Linear_Search_using_Recursion.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSearch(vector<int>& arr, int n, int x) {
+    //Write your code here
+    if(n==arr.size()){
+        return -1;
+    }
+    if(arr[n]==x){
+        return n;
+    }
+    return linearSearch(arr, n+1, x);
+}
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << linearSearch(arr, 0, x) << endl;
+    return 0;
+}
